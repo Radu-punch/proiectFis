28af0715a27a46d06dcc48d05bd382a5840f41b9
Condurari Radu <brockbraun@gmail.com>
2020-03-19 13:15:18 +0200

    modificare noua

--- processors/ProcesorCautare.java
+++ processors/ProcesorCautare.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public class ProcesorCautare implements Procesor {
     private String text;
+    private int k=0;
 
     public ProcesorCautare(String text) {
         this.text = text;
